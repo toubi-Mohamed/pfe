a947266c52dadd78e4241dc85573e839c12cd1a5
toubi-Mohamed <toubi.med1@gmail.com>
2019-05-21 12:43:07 +0200

    ansible script install

--- scripts/ansible-install.sh
+++ scripts/ansible-install.sh
@@ -1,4 +1,16 @@
-apt-get install -y software-properties-common
-apt-add-repository -y ppa:ansible/ansible
+#install with pip 
+apt-get install -y python-pip 
+pip install ansible 
+#pip install --upgrade ansible 
+
+# install with apt 
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
+apt-get install -y ansible 
+
+#install with ppa
+apt-get update 
+apt-get install -y software-properties-common
+apt-add-repository -y --update ppa:ansible/ansible
 apt-get install -y ansible
