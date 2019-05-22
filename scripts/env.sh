d1b72bd3b0554fc371a10f5f3f5f1c253c9f14b5
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-05-22 23:18:23 +0000

    script common utils

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,12 @@
-apt-get install -y software-properties-common
-apt-get install -y vim
-apt-get install -y openssh-server
-apt-get install -y git 
-apt-get install -y wget
-apt-get install -y default-jdk
-apt-get install -y default-jre
-apt-get install -y curl
-apt-get install -y ca-certificates
+apt-get install -y \
+sudo \
+software-properties-common \
+vim \
+openssh-server \
+git \
+wget \
+default-jdk \
+default-jre \
+curl \
+ca-certificates \
 #ssh-keygen -t rsa -N "" -f 
