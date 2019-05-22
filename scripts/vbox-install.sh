d6349258158a789bc1975591a2afa22976ead2d9
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-05-22 23:18:45 +0000

    script vbox install

--- scripts/vbox-install.sh
+++ scripts/vbox-install.sh
@@ -1,10 +1,13 @@
-apt-get install -y \
-sudo \
-software-properties-common \
-curl
+#install virtualbox 
 sudo apt-add-repository 'deb http://download.virtualbox.org/virtualbox/debian stretch contrib'
 sudo curl -O https://www.virtualbox.org/download/oracle_vbox_2016.asc
 sudo apt-key add oracle_vbox_2016.asc
 sudo apt-get update
 sudo apt-get install -y virtualbox-6.0
+rm -r oracle_vbox_2016.asc
+VBoxManage --version
+#install vboxdrv
+#apt-get install -y linux-headers-`uname -r`
+#apt-get install -y dkms
+#/etc/init.d/vboxdrv setup
 
