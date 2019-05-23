5ac1701f602cb918f24763269bf49574394a43c0
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-05-23 09:49:13 +0000

    Update vbox-install.sh

--- scripts/vbox-install.sh
+++ scripts/vbox-install.sh
@@ -1,13 +1,10 @@
 # Add VirtualBox PPA 
 sudo apt-add-repository 'deb http://download.virtualbox.org/virtualbox/debian stretch contrib'
-# Import Sign Key
+# Import Oracle public key
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y virtualbox-6.0
-rm -r \
-oracle_vbox_2016.asc \
-oracle_vbox.asc
 # verification installation of virtualbox 
 VBoxManage --version
 #install vboxdrv
