4e8b2c55ce93f08ed3bd1be172a427eeaaf1183a
toubi-Mohamed <toubi.med1@gmail.com>
2019-05-27 14:04:39 +0200

    avant rapport

---
+++ nexus/ansible/roles/idealista.nexus-role/CHANGELOG.md
@@ -0,0 +1,42 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a changelog](https://github.com/olivierlacan/keep-a-changelog).
+
+## [Unreleased](https://github.com/idealista/nexus-role/tree/develop)
+
+## [1.5.0](https://github.com/idealista/nexus-role/tree/1.5.0) (2019-05-21)
+[Full Changelog](https://github.com/idealista/nexus-role/compare/1.4.0...1.5.0)
+### Added
+- *[#15](https://github.com/idealista/nexus-role/issues/15) Add Alert Email to Scheduled Tasks* @jnogol
+- *[#16](https://github.com/idealista/nexus-role/issues/16) Migrate tests to Molecule 2* @jnogol
+
+### Changed
+- *Update Molecule test Docker image* @jnogol
+
+## [1.4.0](https://github.com/idealista/nexus-role/tree/1.4.0) (2019-05-13)
+[Full Changelog](https://github.com/idealista/nexus-role/compare/1.3.0...1.4.0)
+### Added
+- *[#12](https://github.com/idealista/nexus-role/issues/12) Ability to configure raw repositories* @jnogol
+
+### Fixed
+- *[#11](https://github.com/idealista/nexus-role/issues/11) Nuget and Pypi repositories were configured only if nexus_npm_enable: true* @jnogol
+
+## [1.3.0](https://github.com/idealista/nexus-role/tree/1.3.0) (2018-11-20)
+[Full Changelog](https://github.com/idealista/nexus-role/compare/1.2.0...1.3.0)
+### Added
+- *[#7](https://github.com/idealista/nexus-role/issues/7) Support cleanup policies* @jmonterrubio
+
+## [1.2.0](https://github.com/idealista/nexus-role/tree/1.2.0) (2018-07-18)
+[Full Changelog](https://github.com/idealista/nexus-role/compare/1.1.0...1.2.0)
+### Added
+- *[#4](https://github.com/idealista/nexus-role/issues/4) Enable security realms configuration* @jmonterrubio
+
+## [1.1.0](https://github.com/idealista/nexus-role/tree/1.1.0) (2018-06-07)
+[Full Changelog](https://github.com/idealista/nexus-role/compare/1.0.0...1.1.0)
+### Added
+- *[#2](https://github.com/idealista/nexus-role/issues/2) Add ldap user group* @jmonterrubio
+
+## [1.0.0](https://github.com/idealista/nexus-role/tree/1.0.0) (2018-05-17)
+### Added
+- *First release* @jmonterrubio
---
+++ nexus/ansible/roles/idealista.nexus-role/files/scripts/security/ldap.groovy
@@ -0,0 +1,66 @@
+import org.sonatype.nexus.ldap.persist.LdapConfigurationManager
+import org.sonatype.nexus.ldap.persist.entity.LdapConfiguration
+import org.sonatype.nexus.ldap.persist.entity.Connection
+import org.sonatype.nexus.ldap.persist.entity.Mapping
+import groovy.json.JsonSlurper
+
+parsed_args = new JsonSlurper().parseText(args)
+
+
+def ldapConfigMgr = container.lookup(LdapConfigurationManager.class.getName());
+
+def ldapConfig = new LdapConfiguration()
+boolean update = false;
+
+ldapConfigMgr.listLdapServerConfigurations().each {
+    if (it.name == parsed_args.name) {
+        ldapConfig = it
+        update = true
+    }
+}
+
+ldapConfig.setName(parsed_args.name)
+
+// Connection
+connection = new Connection()
+connection.setHost(new Connection.Host(Connection.Protocol.valueOf(parsed_args.protocol), parsed_args.host, Integer.valueOf(parsed_args.port)))
+if (parsed_args.manager_dn != '' && parsed_args.password != '') {
+    connection.setAuthScheme('simple')
+    connection.setSystemUsername(parsed_args.manager_dn)
+    connection.setSystemPassword(parsed_args.password)
+} else {
+    connection.setAuthScheme('none')
+}
+connection.setSearchBase(parsed_args.search_base)
+connection.setConnectionTimeout(30)
+connection.setConnectionRetryDelay(300)
+connection.setMaxIncidentsCount(3)
+ldapConfig.setConnection(connection)
+
+// Mapping
+mapping = new Mapping()
+mapping.setUserBaseDn(parsed_args.base_dn)
+mapping.setLdapFilter(parsed_args.filter)
+mapping.setUserObjectClass(parsed_args.user_object_class)
+mapping.setUserIdAttribute(parsed_args.user_id_attribute)
+mapping.setUserRealNameAttribute(parsed_args.user_real_name_attribute)
+mapping.setEmailAddressAttribute(parsed_args.user_email_attribute)
+mapping.setUserSubtree(parsed_args.user_subtree)
+
+if (parsed_args.map_groups_as_roles) {
+    mapping.setLdapGroupsAsRoles(true)
+    mapping.setGroupBaseDn(parsed_args.group_base_dn)
+    mapping.setGroupObjectClass(parsed_args.group_object_class)
+    mapping.setGroupIdAttribute(parsed_args.group_id_attribute)
+    mapping.setGroupMemberAttribute(parsed_args.group_member_attribute)
+    mapping.setGroupMemberFormat(parsed_args.group_member_format)
+    mapping.setGroupSubtree(parsed_args.group_subtree)
+}
+
+ldapConfig.setMapping(mapping)
+
+if (update) {
+    ldapConfigMgr.updateLdapServerConfiguration(ldapConfig)
+} else {
+    ldapConfigMgr.addLdapServerConfiguration(ldapConfig)
+}
