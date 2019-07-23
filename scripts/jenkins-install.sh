2c911a011fb0184f90441da88c7702758cfbfcfc
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-07-23 17:04:34 +0100

    Update jenkins-install.sh

--- scripts/jenkins-install.sh
+++ scripts/jenkins-install.sh
@@ -1,3 +1,5 @@
+apt-get install default-jdk
+apt-get install -y apt-transport-https
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ >> /etc/apt/sources.list
 apt-get update
