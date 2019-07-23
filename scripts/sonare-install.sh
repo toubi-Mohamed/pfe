03c4c64e0d0bc4ad532d159afcc667518b9d8c31
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-07-23 17:49:21 +0100

    Update sonare-install.sh

---
+++ scripts/sonare-install.sh
@@ -0,0 +1,6 @@
+apt-get install -y wget \
+unzip
+sudo mkdir /opt/sonarqube
+cd /opt/sonarqube
+wget wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.9.1.zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.9.1.zip
