f4d0be4c92344706b87a1a3b11cf24b83af5900c
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-07-25 11:54:41 +0100

    Create Gitlab-runimage.sh

---
+++ scripts/Gitlab-runimage.sh
@@ -0,0 +1,10 @@
+sudo docker run --detach \
+  --hostname gitlab.example.com \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://my.domain.com/'; gitlab_rails['lfs_enabled'] = true;" \
+  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --name gitlab \
+  --restart always \
+  --volume /srv/gitlab/config:/etc/gitlab \
+  --volume /srv/gitlab/logs:/var/log/gitlab \
+  --volume /srv/gitlab/data:/var/opt/gitlab \
+  gitlab/gitlab-ce:latest
