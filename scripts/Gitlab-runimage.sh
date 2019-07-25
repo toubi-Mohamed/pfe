39829a8a93b60cec0c359bf7766ad3396d4907ad
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-07-25 12:33:24 +0100

    Update Gitlab-runimage.sh

--- scripts/Gitlab-runimage.sh
+++ scripts/Gitlab-runimage.sh
@@ -1,3 +1,4 @@
+# run Gitlab-ce docker image
 sudo docker run --detach \
   --hostname gitlab.example.com \
   --env GITLAB_OMNIBUS_CONFIG="external_url 'http://my.domain.com/'; gitlab_rails['lfs_enabled'] = true;" \
@@ -8,3 +9,11 @@ sudo docker run --detach \
   --volume /srv/gitlab/logs:/var/log/gitlab \
   --volume /srv/gitlab/data:/var/opt/gitlab \
   gitlab/gitlab-ce:latest
+# run Jenkins docker image 
+docker run -d -p 8080:8080 -p 50000:50000 jenkins
+# run sonarqube docker image 
+docker run -d --name sonarqube -p 9000:9000 sonarqube
+# run Nexus docker image 
+$ docker run -d -p 8081:8081 --name nexus sonatype/nexus3
+
+
