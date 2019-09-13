2a902494a2daa1edc36b100b92309c6907e25070
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-09-13 19:05:42 +0100

    Create mysql-cetos.sh

---
+++ scripts/mysql-cetos.sh
@@ -0,0 +1,10 @@
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+md5sum mysql57-community-release-el7-9.noarch.rpm
+rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+yum install mysql-server
+systemctl enable mysqld
+systemctl start mysqld
+systemctl status mysqld
+grep 'temporary password' /var/log/mysqld.log
+ mysql_secure_installation
+ mysqladmin -u root -p version
