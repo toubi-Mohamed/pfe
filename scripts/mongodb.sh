1a28ad812e68b2998ebc876c4cbd48ba88ae1760
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-09-20 13:03:38 +0200

    Update mongodb.sh

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -1,1 +1,13 @@
-
+#import the MongoDB public GPG Key
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#source list for bionic ubuntu
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#source liste for xenial ubuntu
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update
+apt-get install -y mongodb-org
+#file
+#/var/lib/mongodb
+#/var/log/mongodb/mongodb.log
+#/etc/mongob.conf
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
