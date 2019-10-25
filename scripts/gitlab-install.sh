c06dcfb5a91f97ac80290197b8740837b6750fbd
toubi-Mohamed <36959362+toubi-Mohamed@users.noreply.github.com>
2019-10-26 00:20:44 +0100

    Update gitlab-install.sh

--- scripts/gitlab-install.sh
+++ scripts/gitlab-install.sh
@@ -8,6 +8,8 @@ ca-certificates \
 curl
 # install Gitlab 
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+#set variable 
+EXTERNAL_URL="http://git.gitlab.com" 
 apt-get install -y gitlab-ce
 # ficheir de configuration
 /etc/gitlab/gitlab.rb
